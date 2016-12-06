0cb49acd6daf5441c10b67c83febad196ecd29d2
YoueIUT <YoueIUT>
2016-12-06 16:11:05 +0100

    Add Noise FX

---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityStandardAssets.ImageEffects
+{
+    [RequireComponent(typeof(Camera))]
+    public class Noise : PostEffectsBase
+    {
+        [Range(0, 1)]
+        public float intensityMultiplier = 0.25f;
+
+        public Texture2D noiseTexture;
+        Material m_material = null;
+        static float TILE_AMOUNT = 64;
+
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            m_material = new Material(Shader.Find("Hidden/Noise"));
+
+            m_material.SetTexture("_NoiseTex", noiseTexture);
+            m_material.SetFloat("_NoiseAmount", intensityMultiplier);
+
+            RenderTexture.active = destination;
+
+            m_material.SetTexture("_ScreenTex", source);
+
+            float stepSizeX = 1.0f / (source.width / TILE_AMOUNT);
+            float stepSizeY = stepSizeX * source.width / source.height;
+
+            m_material.SetPass(0);
+
+            GL.PushMatrix();
+            GL.LoadOrtho();
+            GL.Begin(GL.QUADS);
+
+            for (float x1 = 0; x1 < 1; x1 += stepSizeX)
+                for (float y1 = 0; y1 < 1; y1 += stepSizeY)
+                {
+                    float XStart = Random.Range(0.0f, 1.0f);
+                    float YStart = Random.Range(0.0f, 1.0f);
+
+                    float texTileSize = (noiseTexture.width / noiseTexture.width) / TILE_AMOUNT;
+
+                    GL.MultiTexCoord2(0, XStart * TILE_AMOUNT, YStart * TILE_AMOUNT);
+                    GL.Vertex3(x1, y1, 0);
+
+                    GL.MultiTexCoord2(0, (XStart + texTileSize) * TILE_AMOUNT, YStart * TILE_AMOUNT);
+                    GL.Vertex3(x1 + stepSizeX, y1, 0);
+
+                    GL.MultiTexCoord2(0, (XStart + texTileSize) * TILE_AMOUNT, (YStart + texTileSize) * TILE_AMOUNT);
+                    GL.Vertex3(x1 + stepSizeX, y1 + stepSizeY, 0);
+
+                    GL.MultiTexCoord2(0, XStart * TILE_AMOUNT, (YStart + texTileSize) * TILE_AMOUNT);
+                    GL.Vertex3(x1, y1 + stepSizeY, 0);
+                }
+
+            GL.End();
+            GL.PopMatrix();
+        }
+    }
+}
---
+++ Assets/Shaders/Noise.shader
@@ -0,0 +1,69 @@
+﻿Shader "Hidden/Noise"
+{	
+	SubShader
+	{
+		ZTest Always Cull Off ZWrite Off Blend Off
+	  
+		Pass
+		{
+			CGPROGRAM
+		
+				#pragma vertex vert
+				#pragma fragment frag
+
+				#include "UnityCG.cginc"
+
+				sampler2D _ScreenTex;
+				sampler2D _NoiseTex;
+
+				uniform float _NoiseAmount;
+
+				struct vinput 
+				{
+					float4 vertex : POSITION;
+					float2 texcoord : TEXCOORD0; // Offset
+				};
+
+				struct v2f 
+				{
+					float4 pos : SV_POSITION;
+					float2 uv_screen : TEXCOORD0;
+					float4 uv_noise : TEXCOORD1;
+				};						
+
+				inline float3 Overlay(float3 m, float3 color) {
+					color = saturate(color);
+					float3 check = step(float3(0.5,0.5,0.5), color.rgb);
+					float3 result = check * (float3(1,1,1) - ((float3(1,1,1) - 2*(color.rgb-0.5)) * (1-m.rgb))); 
+					result += (1-check) * (2*color.rgb) * m.rgb;
+					return result;
+				}
+				
+				v2f vert (vinput v)
+				{
+					v2f o;
+			
+					o.pos = mul (UNITY_MATRIX_MVP, v.vertex);	
+			
+					// Background Pixel
+					o.uv_screen = float2(v.vertex.x,  v.vertex.y);
+					// Noise Tiling
+					o.uv_noise = v.texcoord.xyxy;
+
+					return o; 
+				}
+
+				float4 frag ( v2f i ) : SV_Target
+				{
+					float4 oldColor = (tex2D (_ScreenTex, i.uv_screen));
+			
+					float3 newColor = (tex2D(_NoiseTex, i.uv_noise.xy) * float4(1,1,1,0)).rgb;
+					newColor = lerp(float3(0.5,0.5,0.5), newColor, _NoiseAmount);
+
+					return float4(Overlay(newColor, oldColor.rgb), oldColor.a);
+				}
+
+			ENDCG
+		}	
+	}
+}
