c871fdf68336e3ce335476101c00cde08fb65ed2
YoueIUT <YoueIUT>
2016-12-13 10:52:13 +0100

    Wave follow camera

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(Camera.main.transform);
+        transform.rotation *= Quaternion.Euler(90, 0, 0);
+	}
+}
