29fed861d93f98b775332fedbd302791f6c6fbf7
YoueG <YoueG>
2017-02-10 11:53:25 +0100

    Clean Unity

--- Unity 5.4.2/Data/MonoBleedingEdge/bin/cli.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-SETLOCAL
-set MONO_PREFIX=%~dp0..
-set MONO=%MONO_PREFIX%\bin\mono
-set MONO_CFG_DIR=%MONO_PREFIX%\etc
-"%MONO%" %*
-exit /b %ERRORLEVEL%
-ENDLOCAL
--- Unity 5.4.2/Data/MonoBleedingEdge/bin/resgen2.bat
+++
@@ -1,2 +0,0 @@
-@"%~dp0cli.bat" %MONO_OPTIONS% "%~dp0..\lib\mono\2.0\resgen.exe" %*
-exit /b %ERRORLEVEL%
